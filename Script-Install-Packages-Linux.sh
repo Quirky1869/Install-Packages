c5f64631c8c0a4d592a8728f754ca4d75d4f33d2
Quirky1869 <mickael.jacotet@gmail.com>
2024-04-07 22:21:38 +0200

    Update Script-Install-Packages-Linux.sh V2
    Ajout des logs dans /var/log/Script-Install-Packages-Linux.sh
    Correction des imports de themes, icons, .icons
    Ajout choix fermer session après execution du script
    Suppression raccourci clavier custom 6 (explorateur de fichiers)
    Ajout de gnome-shell-extensions, pip, make, notify-send
    Ajout de pop-up avec notify-end durant le script

--- Script-Install-Packages-Linux.sh
+++ Script-Install-Packages-Linux.sh
@@ -1,35 +1,101 @@
 #!/bin/bash
 
-echo "Debut de l'installation :"
-echo ===========================
+# Emplacement fichier log
+LOG_FILE="/var/log/Script-install-Packages-Linux.sh.log"
+
+# Fonction pour gérer l'interruption (Ctrl+C)
+interrupt_handler() {
+    # echo "Suppression du dossier Necessary..."
+    # rm -rf "/home/$USER/Necessary"
+    echo "Interruption de l'exécution du script."
+    exit 1
+}
+
+# SIGINT = CTRL + C
+trap interrupt_handler SIGINT
+
+echo "Debut de l'installation :" 
+echo =========================== 
 
 # Mise à jour et upgrade
-sudo apt update -y && sudo apt upgrade -y
+sudo apt update -y && sudo apt upgrade -y 
 
-# Installation des packets
-echo Installation des packets
-sudo apt install -y tree wget recon-ng htop filezilla zip unzip rar unar unrar net-tools \
-    bmon tcptrack nmap whois wireshark testdisk tshark git samba gnome-tweaks python3-impacket \
-    gnome-disk-utility gparted xournal netdiscover dirb hydra netcat pluma bpytop edb-debugger
-
-# Ajout groupe Wireshark
+# Wireshark
+sudo apt install -y wireshark
 echo "Ajout de l'utilisateur au groupe "wireshark" :"
 sudo adduser $USER wireshark
 
+# Choix installation de GNS3
+choixGNS3="" 
+
+while [[ "$choixGNS3" != "y" && "$choixGNS3" != "n" ]]; do 
+
+echo "Voulez-vous installer GNS3 ? ( y / n ) :" 
+read choixGNS3 
+
+case $choixGNS3 in 
+    y)  # Si le choix est oui
+        echo "Vous avez choisi d'installer GNS3 à vos risques et périls" 
+        cd ~/Documents 
+        sudo add-apt-repository ppa:gns3/ppa 
+        sudo apt update 
+        sudo apt install gns3-gui gns3-server libpcap-dev -y 
+        git clone https://github.com/GNS3/ubridge.git -4 
+        cd ubridge 
+        sudo apt install -y gcc-multilib 
+        make 
+        sudo make install 
+
+        echo "Si vous n'avez pas les VPCs dans GNS3, allez dans ~/Documents et executer le fichier install-dynamips.sh" 
+        notify-send "Si vous n'avez pas les VPCs dans GNS3, allez dans ~/Documents et executer le fichier install-dynamips.sh" 
+        cd ~/Documents 
+        touch install-dynamips.sh 
+        echo '#!/bin/bash' >> install-dynamips.sh 
+        echo 'sudo apt install -y dynamips' >> install-dynamips.sh 
+        echo 'git clone https://github.com/TolgaBagci/vpcs_0.8-1_amd64.deb.git' >> install-dynamips.sh 
+        echo 'cd vpcs_0.8-1_amd64.deb' >> install-dynamips.sh 
+        echo 'sudo dpkg -i vpcs_0.8-1_amd64.deb' >> install-dynamips.sh 
+        sudo chmod +x install-dynamips.sh 
+        cd 
+        ;;
+    n)  # Si le choix est non
+        echo "Vous n'avez pas choisi d'installer GNS3, ouf !!!" 
+        ;;
+    *)  # Si aucun choix ne correspond
+        echo "Ta pas fait le bon choix Maurice (Attention à la casse)" 
+        ;;
+esac
+done
+
+# Redirection des log après installation de GNS3 et Wireshark
+exec > >(sudo tee -a "$LOG_FILE") 2>&1
+
+# Installation des packets
+echo Installation des packets 
+sudo apt install -y tree wget recon-ng htop filezilla zip unzip rar unar unrar net-tools \
+    bmon tcptrack nmap whois testdisk tshark git samba gnome-tweaks python3-impacket \
+    gnome-disk-utility gparted xournal netdiscover dirb hydra netcat pluma bpytop edb-debugger pip \
+    make gnome-shell-extensions 
+
+sudo pip install -U notify-send
+
 # Import Theme Terminal
-dconf load /org/gnome/terminal/legacy/profiles:/ < ./Necessary/gnome-terminal-profiles.dconf
+dconf load /org/gnome/terminal/legacy/profiles:/ < ./Necessary/gnome-terminal-profiles.dconf 
 
 # Import Modeles
-cp ./Necessary/Nouveaux-documents/* /home/$USER/Modèles
+cp -r ./Necessary/Nouveaux-documents/* /home/$USER/Modèles 
 
 # Import Themes
-cp ./Necessary/Themes/* /home/$USER/.themes/
+mkdir /home/$USER/.themes/ 
+cp -r ./Necessary/Themes/* /home/$USER/.themes/ 
 
 # Import Share Icons
-cp ./Necessary/Share-Icons/* /home/$USER/.local/share/icons/
+mkdir /home/$USER/.local/share/icons/ 
+cp -r ./Necessary/Share-Icons/* /home/$USER/.local/share/icons/ 
 
 # Import Icons
-cp ./Necessary/Icons/* /home/$USER/.icons/
+mkdir /home/$USER/.icons/ 
+cp -r ./Necessary/Icons/* /home/$USER/.icons/ 
 
 # Choix install Google Chrome
 choixGoogleChrome=""
@@ -39,7 +105,7 @@ echo "Voulez-vous installer Google Chrome ? ( y  /  n ) :"
 read choixGoogleChrome
 
 case $choixGoogleChrome in
-    y )  # Si le choix est oui
+    y)  # Si le choix est oui
         cd ~/Téléchargements
         wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
         echo Installation de Google Chrome :
@@ -47,7 +113,7 @@ case $choixGoogleChrome in
         sudo rm ./*deb
         cd
         ;;
-    n )  # Si le choix est non
+    n)  # Si le choix est non
         echo "Vous n'avez pas choisi d'installer Google Chrome"
         ;;
     *)  # Si aucun choix ne correspond
@@ -64,7 +130,7 @@ echo "Voulez-vous installer Visual Studio Code ? ( y  /  n ) :"
 read choixVisualStudioCode
 
 case $choixVisualStudioCode in
-    y )  # Si le choix est oui
+    y)  # Si le choix est oui
         echo Telechargement de Visual Studio Code :
         cd ~/Téléchargements
         wget --content-disposition https://go.microsoft.com/fwlink/?LinkID=760868
@@ -73,7 +139,7 @@ case $choixVisualStudioCode in
         sudo rm ./*deb
         cd
         ;;
-    n )  # Si le choix est non
+    n)  # Si le choix est non
         echo "Vous n'avez pas choisi d'installer Visual Studio Code"
         ;;
     *)  # Si aucun choix ne correspond
@@ -90,7 +156,7 @@ echo "Voulez-vous installer Discord ? ( y  /  n ) :"
 read choixDiscord
 
 case $choixDiscord in
-    y )  # Si le choix est oui
+    y)  # Si le choix est oui
         echo "Vous avez choisi d'installer Discord"
         cd ~/Téléchargements
         wget https://dl.discordapp.net/apps/linux/0.0.47/discord-0.0.47.deb
@@ -99,49 +165,8 @@ case $choixDiscord in
 	sudo rm ./*deb
 	cd
         ;;
-    n )  # Si le choix est non
-        echo "Vous n'avez pas choisi d'installer Discord"
-        ;;
-    *)  # Si aucun choix ne correspond
-        echo "Ta pas fait le bon choix Maurice (Attention à la casse)"
-        ;;
-esac
-done
-
-# Choix installation de GNS3
-choixGNS3=""
-
-while [[ "$choixGNS3" != "y" && "$choixGNS3" != "n" ]]; do
-
-echo "Voulez-vous installer GNS3 ? ( y / n ) :"
-read choixGNS3
-
-case $choixGNS3 in
-    y)  # Si le choix est oui
-        echo "Vous avez choisi d'installer GNS3 à vos risques et périls"
-        cd ~/Documents
-        sudo add-apt-repository ppa:gns3/ppa
-        sudo apt update
-        sudo apt install gns3-gui gns3-server libpcap-dev -y
-        git clone https://github.com/GNS3/ubridge.git -4
-        cd ubridge
-        sudo apt install -y gcc-multilib
-        make
-        sudo make install
-
-        echo "Si vous n'avez pas les VPCs dans GNS3, allez dans ~/Documents et executer le fichier install-dynamips.sh"
-        cd ~/Documents
-        touch install-dynamips.sh
-        echo '#!/bin/bash' >> install-dynamips.sh
-        echo 'sudo apt install -y dynamips' >> install-dynamips.sh
-        echo 'git clone https://github.com/TolgaBagci/vpcs_0.8-1_amd64.deb.git' >> install-dynamips.sh
-        echo 'cd vpcs_0.8-1_amd64.deb' >> install-dynamips.sh
-        echo 'sudo dpkg -i vpcs_0.8-1_amd64.deb' >> install-dynamips.sh
-        sudo chmod +x install-dynamips.sh
-        cd
-        ;;
     n)  # Si le choix est non
-        echo "Vous n'avez pas choisi d'installer GNS3, ouf !!!"
+        echo "Vous n'avez pas choisi d'installer Discord"
         ;;
     *)  # Si aucun choix ne correspond
         echo "Ta pas fait le bon choix Maurice (Attention à la casse)"
@@ -157,11 +182,11 @@ echo "Voulez-vous installer Apache2 ? ( y /  n ) :"
 read choixApache2
 
 case $choixApache2 in
-    y )  # Si le choix est oui
+    y)  # Si le choix est oui
         echo "Vous avez choisi d'installer Apache2"
         sudo apt install -y apache2
         ;;
-    n )  # Si le choix est non
+    n)  # Si le choix est non
         echo "Vous n'avez pas choisi d'installer Apache2"
         ;;
     *)  # Si aucun choix ne correspond
@@ -178,11 +203,11 @@ echo "Voulez-vous installer mariadb ? ( y  /  n ) :"
 read choixmariadb
 
 case $choixmariadb in
-    y )  # Si le choix est oui
+    y)  # Si le choix est oui
         echo "Vous avez choisi d'installer mariadb"
         sudo apt install -y mariadb-server
         ;;
-    n )  # Si le choix est non
+    n)  # Si le choix est non
         echo "Vous n'avez pas choisi d'installer mariadb"
         ;;
     *)  # Si aucun choix ne correspond
@@ -201,7 +226,7 @@ echo "Voulez-vous installer Docker Desktop ? ( y  /  n ) :"
 read choixDockerDesktop
 
 case $choixDockerDesktop in
-    y )  # Si le choix est oui
+    y)  # Si le choix est oui
         echo "Vous avez choisi d'installer Docker Desktop"
         #sudo apt install -y curl docker-ce-cli pass uidmap
         cd ~/Téléchargements
@@ -215,14 +240,14 @@ case $choixDockerDesktop in
         sudo apt install -y docker-compose-plugin
         echo "Etat actuel de Docker"
         sudo systemctl is-active docker
-        sleep 10
+        sleep 5
         wget https://desktop.docker.com/linux/main/amd64/137060/docker-desktop-4.27.2-amd64.deb
         sudo apt install ./*.deb
         sudo rm ./*deb
         cd
         sudo systemctl restart docker
         ;;
-    n )  # Si le choix est non
+    n)  # Si le choix est non
         echo "Vous n'avez pas choisi d'installer Docker Desktop"
         ;;
     *)  # Si aucun choix ne correspond
@@ -258,7 +283,7 @@ echo "Voulez-vous installer GitHub Desktop ? ( y  /  n ) :"
 read choixGithubDesktop
 
 case $choixGithubDesktop in
-    y )  # Si le choix est oui
+    y)  # Si le choix est oui
         echo "Vous avez choisi d'installer GitHub Desktop"
         sudo apt install -y apt-transport-https gnupg2 software-properties-common
         cd ~/Téléchargements
@@ -267,7 +292,7 @@ case $choixGithubDesktop in
         rm ./*.deb
 	cd
         ;;
-    n )  # Si le choix est non
+    n)  # Si le choix est non
         echo "Vous n'avez pas choisi d'installer GitHub Desktop"
         ;;
     *)  # Si aucun choix ne correspond
@@ -284,7 +309,7 @@ echo "Voulez-vous installer NordVPN ? ( y  /  n ) :"
 read choixNordVPN
 
 case $choixNordVPN in
-    y )  # Si le choix est oui
+    y)  # Si le choix est oui
         echo "Vous avez choisi d'installer NordVPN"
         cd ~/Téléchargements
         wget -c https://repo.nordvpn.com/deb/nordvpn/debian/pool/main/nordvpn-release_1.0.0_all.deb
@@ -295,11 +320,11 @@ case $choixNordVPN in
         # nordvpn login -> copier coller le lien obtenu dans un browser et se connecter
         # nordvpn countries -> lister les pays disponible
         # nordvpn c Belgium -> se connecter en belgique
-        # nordvpn d -> se deconnecter
+        # nordvpn d -> se deconnecter
         sudo rm ./*.deb
 	cd
         ;;
-    n )  # Si le choix est non
+    n)  # Si le choix est non
         echo "Vous n'avez pas choisi d'installer NordVPN"
         ;;
     *)  # Si aucun choix ne correspond
@@ -309,7 +334,7 @@ esac
 done
 
 # Création des raccourcis clavier
-gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom4/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom5/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom6/']" # Création des emplacements personnalisés des raccourcis claviers
+gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom4/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom5/']" # Création des emplacements personnalisés des raccourcis claviers
 
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name 'Discord' # Nom du raccourci
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command 'discord' # Commande du raccourci
@@ -335,24 +360,63 @@ gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/or
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom5/ command 'gnome-terminal' # Commande du raccourci
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom5/ binding '<Alt>t' # Raccourci clavier attribué
 
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom6/ name 'Explorateur de fichiers' # Nom du raccourci
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom6/ command 'Nautilus' # Commande du raccourci
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom6/ binding '<Super>e' # Raccourci clavier attribué
-
 # Raccourci claviers non personnalisés
 echo "Pensez à configurer les raccourcis clavier non personnalisés"
 echo "Capture d'écran -> Effectuer une capture d'écran interactivement -> Maj + Super + S"
 echo "Navigation -> Changer de fenêtre directement -> Alt + Tabulation"
 echo "Lanceurs -> Dossier Personnel -> Super + E"
 echo "Lanceurs -> Lancer un terminal -> Alt + T"
-Pause
+sleep 10
 
-#Pause
-echo Installation terminée la session va se fermer
+# Suppression du dossier "Necessary"
+notify-send "Suppression du dossier Necessary" -t 3000
+rm -rf "/home/$USER/Necessary"
+
+# Pause
+echo Installation terminée il faut fermer la session pour appliquer tous les paramètres
+notify-send -u critical "Installation terminée il faut fermer la session pour appliquer tous les paramètres" -t 4000
 sleep 3
 
+# Choix Logoff
+choixLogoff=""
+
+while [[ "$choixLogoff" != "y" && "$choixLogoff" != "n" ]]; do
+echo "Voulez-vous fermer la session maintenant ? ( y  /  n ) :"
+read choixLogoff
+
+case $choixLogoff in
+    y)  # Si le choix est oui
+        echo "Vous avez choisi de fermer la session"
+        sleep 1
+        echo "La session va se fermer dans 5 secondes"
+        sleep 1
+        echo "La session va se fermer dans 4 secondes"
+        sleep 1
+        echo "La session va se fermer dans 3 secondes"
+        sleep 1
+        echo "La session va se fermer dans 2 secondes"
+        sleep 1
+        echo "La session va se fermer dans 1 seconde"
+        sleep 1
+        skill -KILL -u $USER
+        ;;
+    n)  # Si le choix est non
+        echo "Vous n'avez pas choisi de fermer la session"
+        notify-send "Vous n'avez pas choisi de fermer la session" -t 5000
+        echo "D'accord BYE !!!"
+        sleep 4
+        ;;
+    *)  # Si aucun choix ne correspond
+        echo "Ta pas fait le bon choix Maurice (Attention à la casse)"
+        ;;
+esac
+done
+
+# Compiz wobbly windows effect :
+# https://extensions.gnome.org/extension/3210/compiz-windows-effect/
+
+
 # Logoff
-echo Fermeture de session pour application de tout les paramètres :
-sleep 2
-pkill -u $USER
+# echo Fermeture de session pour application de tout les paramètres :
+# pkill -u $USER
 # skill -KILL -u $USER
