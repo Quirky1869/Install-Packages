b82e6f9cdc05db8add5e98070988e856526fafcb
Quirky <48966267+Quirky1869@users.noreply.github.com>
2024-04-09 22:36:13 +0200

    Update Script-Install-Packages-Linux.sh V2.1
    Ajout de l'installation d'Obsidian
    Configuration en partie de rclone
    Ajout du raccourci clavier <ctrl> + <alt> + o pour la synchro obsidian -> Google-drive
    Ajout d'un mémo.txt
    Ajout du script "synchro-obsidian-google-drive.sh" copier dans /user/local/bin

--- Script-Install-Packages-Linux.sh
+++ Script-Install-Packages-Linux.sh
@@ -350,7 +350,8 @@ case $choixObsidian in
         sudo cp -f ./Necessary/Obsidian-rclone/synchro-obsidian-google-drive.sh /usr/local/bin
         sudo chmod u+x /usr/local/bin/synchro-obsidian-google-drive.sh
         sudo chown $USER:$USER -R /usr/local/bin/synchro-obsidian-google-drive.sh
-        notify-send -i dialog-information "Obsidian installé avec succès" "Il faudra finaliser le paramètrage de rclone" -t 4000
+        notify-send -i dialog-information "Obsidian installé avec succès" "Il faudra finaliser le paramètrage de rclone" -t 2000
+	notify-send -i face-wink "Rclone" "Regarder le fichier memo.txt pour configurer rclone" -t 4000
 	cd
         ;;
     n)  # Si le choix est non
