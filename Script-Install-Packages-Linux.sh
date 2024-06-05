1c95b013afe76a038c72a97759668c2bd185395d
Quirky <48966267+Quirky1869@users.noreply.github.com>
2024-06-05 08:21:37 +0200

    Update Script-Install-Packages-Linux.sh V3.1.9
    Correction docker

--- Script-Install-Packages-Linux.sh
+++ Script-Install-Packages-Linux.sh
@@ -305,6 +305,7 @@ case $choixDockerDesktop in
         sudo apt install apt-transport-https ca-certificates curl gnupg
         curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker.gpg
         echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu jammy stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+	sudo apt update -y
         sudo apt install -y docker-ce 
         sudo apt install -y docker-ce-cli
         sudo apt install -y containerd.io
