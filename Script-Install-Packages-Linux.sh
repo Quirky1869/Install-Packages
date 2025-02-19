67ac758f92e48c1cba6b1dedef7ab3f9f802184e
Quirky <48966267+Quirky1869@users.noreply.github.com>
2025-02-19 10:22:14 +0100

    Update Script-Install-Packages-Linux.sh

--- Script-Install-Packages-Linux.sh
+++ Script-Install-Packages-Linux.sh
@@ -765,6 +765,8 @@ alias servicesystemd="systemctl list-unit-files" # S'appui sur systemd
 alias whatfilemanager="xdg-mime query default inode/directory"
 alias whatenvironment="echo $XDG_CURRENT_DESKTOP"
 alias pythonserver="sudo python3 -m http.server"
+
+alias lazydocker="/home/$USER/.local/bin/lazydocker" # S'appui sur LazyDocker
 EOF
 
         cp -f ./Necessary/bashrc/.bashrc* /home/$USER/
