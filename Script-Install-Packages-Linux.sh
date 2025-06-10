442658c6699f4e4b31bcfa281534c23b77effc9f
Quirky <48966267+Quirky1869@users.noreply.github.com>
2025-06-10 15:00:19 +0200

    Update Script-Install-Packages-Linux.sh
    add ncdu

--- Script-Install-Packages-Linux.sh
+++ Script-Install-Packages-Linux.sh
@@ -157,7 +157,7 @@ sudo apt install -y tree wget netcat-traditional htop filezilla zip unzip rar un
     gnome-disk-utility gparted xournal netdiscover dirb hydra pluma bpytop edb-debugger pip \
     make gnome-shell-extensions rclone rclone-browser dsniff tcpdump libfuse2 pv curl network-manager \
     diodon bat john flameshot binwalk hashcat libimage-exiftool-perl baobab pipx asciinema tmux drawing micro \
-    freerdp2-x11 pinta hexcurse gnome-browser-connector
+    freerdp2-x11 pinta hexcurse gnome-browser-connector ncdu
 
 # pip
 sudo pip install -U notify-send
