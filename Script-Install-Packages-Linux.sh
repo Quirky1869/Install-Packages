20b65f0acdc9661bffeb9ad95a902d712b2c4091
Quirky <48966267+Quirky1869@users.noreply.github.com>
2025-09-30 21:07:11 +0200

    Update Script-Install-Packages-Linux.sh

--- Script-Install-Packages-Linux.sh
+++ Script-Install-Packages-Linux.sh
@@ -792,7 +792,10 @@ read choixKittyTerminal
 
 case $choixKittyTerminal in
     y)  # Si le choix est oui)
+		# Instller ou mettre à jour Kitty
         curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+
+		
         sudo ln -sf ~/.local/kitty.app/bin/kitty ~/.local/kitty.app/bin/kitten /usr/local/bin
 
         cp ~/.local/kitty.app/share/applications/kitty.desktop ~/.local/share/applications/
